4d9e09cc55af45d387f8148c5ea18bb9b67291fa
DESKTOP-69LND0I\38096 <zoyatsm@gmail.com>
2025-04-25 17:52:04 +0300

    fix email

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -115,7 +115,7 @@ test('Create article - it should be created', async ({ request }) => {
   //AAA
   //Arrange
   const authRequestBody = {
-    user:{email:"testuser2@mail.com",password:"1234"}  
+    user:{email:"testuser4@mail.com",password:"1234"}  
 };
 
 const authResponse = await request.post('/api/users/login', { data: authRequestBody  
